42033fd5dd73086ebc3dbc2baf6af487b087dcc8
Jose A González <jagascripts@gmail.com>
2025-08-12 20:28:18 +0200

    style(ui): format styles and section buttons after icon update

--- prompt-builder/src/app/features/sections/sections-list.component.ts
+++ prompt-builder/src/app/features/sections/sections-list.component.ts
@@ -41,7 +41,10 @@ import { TemplatesStore } from '../../core/store/templates.store';
                 (click)="moveUp(s.key)"
                 [attr.aria-label]="'Move ' + s.title + ' up'"
               >
-                <span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span>
+                <span
+                  class="glyphicon glyphicon-chevron-up"
+                  aria-hidden="true"
+                ></span>
               </button>
               <button
                 type="button"
@@ -49,7 +52,10 @@ import { TemplatesStore } from '../../core/store/templates.store';
                 (click)="moveDown(s.key)"
                 [attr.aria-label]="'Move ' + s.title + ' down'"
               >
-                <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>
+                <span
+                  class="glyphicon glyphicon-chevron-down"
+                  aria-hidden="true"
+                ></span>
               </button>
             </div>
           </div>
